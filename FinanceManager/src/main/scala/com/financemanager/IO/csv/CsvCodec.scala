8c4de9fa5209dc487b0adb956d388f34aa585aaa
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2026-06-06 22:15:42 +0200

    Remove legacy CSV code

--- FinanceManager/src/main/scala/com/financemanager/IO/csv/CsvCodec.scala
+++
@@ -1,9 +0,0 @@
-package com.financemanager.IO.csv
-
-import com.financemanager.domain.error.IOError
-
-trait CsvCodec[T]:
-  def headers: Seq[String]
-  def encode(value: T): Seq[String]
-  def decode(fields: Seq[String]): Either[IOError, T]
-
